b975e14fa98e56d333561abd7f136979ba2a3939
Sean Conrad <seanrad@Seans-MacBook-Pro.local>
2018-10-01 23:12:06 -0400

    edited a serializer

--- app/serializers/skate_spot_serializer.rb
+++ app/serializers/skate_spot_serializer.rb
@@ -1,4 +1,5 @@
 class SkateSpotSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :country, :state, :city, :longitude, :latitude, :skatephoto, :user_id
   belongs_to :user
+  has_many :likes
 end
