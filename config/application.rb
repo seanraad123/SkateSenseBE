1f295c0f43573fb81082baf89a53214fd3a17f67
Sean Conrad <seanrad@Seans-MacBook-Pro.local>
2018-10-09 18:29:55 -0400

    make some changes for cors

--- config/application.rb
+++ config/application.rb
@@ -24,10 +24,12 @@ module SkateSenseBE
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+            :headers => :any,
+            :methods => [:get, :post, :delete, :put, :options]
       end
     end
 
