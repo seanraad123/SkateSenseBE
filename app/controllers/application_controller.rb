a4895100635a851680016cde83e9646f41943621
Sean Conrad <seanrad@Seans-MacBook-Pro.local>
2018-10-09 18:35:08 -0400

    edited a few fuctions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
-before_filter :cors_preflight_check
-after_filter :cors_set_access_control_headers
+before_action :cors_preflight_check
+after_action :cors_set_access_control_headers
 
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
