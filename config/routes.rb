2b516237e7b3ef3a95ff0ffe34c684ad803005bc
Sean Conrad <seanrad@Seans-MacBook-Pro.local>
2018-10-09 18:33:54 -0400

    removed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      match "*all" => "application#cors_preflight_check", :constraints => { :method => "OPTIONS" }
       resources :skate_spots, only: [:index, :create, :show, :destroy]
       resources :users, only: [:index, :show, :create, :destroy]
       resources :likes, only: [:index, :create, :show, :destroy]
