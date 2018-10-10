7612d32d893fc732bb47333741db32927446a65e
seanrad <seanconrad123@gmail.com>
2018-10-09 22:13:11 -0400

    edited app file

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module SkateSenseBE
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'localhost:3000', 'http://seanconrad.me:3000'
+        origins 'localhost:3000', 'http://seanconrad.me'
         resource '/api', :headers => :any, :methods => :any
       end
     end
