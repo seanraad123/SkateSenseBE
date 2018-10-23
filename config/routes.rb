bcd4d1b331cc0325fa07beef46efbf7cc19d3f95
seanrad <seanconrad123@gmail.com>
2018-10-23 16:22:38 -0400

    implemented JWT tokens

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :skate_spots, only: [:index, :create, :show, :destroy]
-      resources :users, only: [:index, :show, :create, :destroy]
+      resources :users, only: [:index, :show, :create, :destroy, :profile]
       resources :likes, only: [:index, :create, :show, :destroy]
       resources :bookmarks, only: [:index, :create, :show, :destroy]
       post '/login', to: 'auth#create'
