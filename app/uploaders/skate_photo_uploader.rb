a725b3c0c216b13ff288d3c68ba9de6e4acdc302
seanrad <seanconrad123@gmail.com>
2019-01-27 01:32:09 -0500

    removed resize_to_fit

--- app/uploaders/skate_photo_uploader.rb
+++ app/uploaders/skate_photo_uploader.rb
@@ -31,9 +31,9 @@ class SkatePhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process resize_to_fit: [279, 209]
-  end
+  # version :thumb do
+  #   process resize_to_fit: [279, 209]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
