a0f95be947d7e07b0fe458371e2e3c12a06956d7
seanrad <seanconrad123@gmail.com>
2019-01-27 00:14:03 -0500

    added photo resizing

--- app/uploaders/skate_photo_uploader.rb
+++ app/uploaders/skate_photo_uploader.rb
@@ -21,6 +21,8 @@ class SkatePhotoUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
+
+
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
@@ -29,9 +31,9 @@ class SkatePhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [150, 150]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
