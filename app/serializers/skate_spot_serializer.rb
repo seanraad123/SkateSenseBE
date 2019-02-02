2129b222d9434b0c715a19ba83af78928217c9ca
seanrad <seanconrad123@gmail.com>
2019-02-02 00:49:41 -0500

    added comments to my serializer

--- app/serializers/skate_spot_serializer.rb
+++ app/serializers/skate_spot_serializer.rb
@@ -2,4 +2,5 @@ class SkateSpotSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :country, :state, :city, :longitude, :latitude, :skatephoto, :user_id
   belongs_to :user
   has_many :likes
+  has_many :comments
 end
