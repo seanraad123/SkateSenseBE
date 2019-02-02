451678515e7f15010b0e4c5bbd2c6503ca327ddc
seanrad <seanconrad123@gmail.com>
2019-02-02 01:55:49 -0500

    added create and destroy methods to comments controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -10,4 +10,26 @@ class Api::V1::CommentsController < ApplicationController
     render json: comment
   end
 
+  def create
+    @comment =  Comment.new(comment_params)
+    if @comment.save
+      render json: @comment, status: 201
+    else
+      render json: @comment.errors.messages, status: 400
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      render json: { status: 200, msg: 'Comment has been deleted.' }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.permit(:content, :user_id, :skate_spot_id)   #These must be included in the body of the POST or PATCH requests we will be making with JS fetch.
+  end
+
 end
