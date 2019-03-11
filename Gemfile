2de05a38e7c2e4b858eb82f8db6585b1137f0601
seanrad <seanconrad123@gmail.com>
2019-03-11 15:57:24 -0400

    photos are now being compressed

--- Gemfile
+++ Gemfile
@@ -21,7 +21,7 @@ gem 'carrierwave', '~> 1.0'
 gem "active_model_serializers", "~> 0.10.7"
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,7 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -24,9 +24,9 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
-  def scale(width, height)
-    process scale: [200, 300]
-  end
+  # process :quality => 50
+
+  process :resize_to_fit => [500, 500]
 
   # Create different versions of your uploaded files:
   # version :thumb do
